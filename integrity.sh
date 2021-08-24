f9fde66ab25612f0d5c05b78eb67794fd6ec47a5
leighpoulier <86460742+leighpoulier@users.noreply.github.com>
2021-08-24 10:41:13 +1000

    comment out redundant code, add check for program resources

--- integrity.sh
+++ integrity.sh
@@ -11,38 +11,36 @@ function usage() {
   echo -e "  -h\tPrint this help."
 }
 
-UP="\033[1A"      #Move Cursor UP
-EF="\033[0K"      #Erase Forward
-GN="\033[01;32m"  #Set Colour Green
-RD="\033[01;31m"  #Set Colour Red
-GY="\033[00;36m"  #Set Colour Grey
-NC="\033[00m"     #Set No Colour
-WH="\033[01;37m"  #Set Colour White
+# UP="\033[1A"      #Move Cursor UP
+# EF="\033[0K"      #Erase Forward
+# GN="\033[01;32m"  #Set Colour Green
+# RD="\033[01;31m"  #Set Colour Red
+# GY="\033[00;36m"  #Set Colour Grey
+# NC="\033[00m"     #Set No Colour
+# WH="\033[01;37m"  #Set Colour White
 newLine=$'\n'
 
 
 _V=0
-_INDENT=10#0
-_OLDINDENT=10#0
+# _INDENT=10#0
+# _OLDINDENT=10#0
 function log () {
     if [[ $_V -eq 1 ]]; then
-      if [[ -n "$_INDENT" ]] && [[ -n "$_OLDINDENT" ]]; then
-        if [[ "$_INDENT" -ne "$_OLDINDENT" ]]; then
-          echo ""
-        fi
-        for (( i = 0; i < $_INDENT; i++ )); do
-          echo -n "  "
-        done
-      fi
-      echo -e "${GY}$@${NC}"
+      # if [[ -n "$_INDENT" ]] && [[ -n "$_OLDINDENT" ]]; then
+      #   if [[ "$_INDENT" -ne "$_OLDINDENT" ]]; then
+      #     echo ""
+      #   fi
+      #   for (( i = 0; i < $_INDENT; i++ )); do
+      #     echo -n "  "
+      #   done
+      # fi
+      # echo -e "${GY}$@${NC}"
+      echo -e "$@"
       _OLDINDENT="$_INDENT"
     fi
 }
 
-
 allowedDigestValues=("256" "384" "512")
-
-
 _DIGEST=512
 
 # if ! ([[ $# -eq 2 ]] || [[ $# -eq 1 ]])
@@ -82,8 +80,28 @@ do
 done
 shift $((OPTIND -1))
 
-log "_DIGEST=$_DIGEST"
+if ! command -v shasum &> /dev/null
+then
+    echo "shasum could not be found"
+    exit 1
+fi
+if ! command -v awk &> /dev/null
+then
+    echo "awk could not be found"
+    exit 1
+fi
+if ! command -v xxd &> /dev/null
+then
+    echo "xxd could not be found"
+    exit 1
+fi
+if ! command -v base64 &> /dev/null
+then
+    echo "base64 could not be found"
+    exit 1
+fi
 
+log "_DIGEST=$_DIGEST"
 log "Number of arguments: $#"
 
 for file in "$@"
